3342185cd31ef1fa93e9a70a649bb0bfe88f60db
1grim1 <dima.ggrimbergg@gmail.com>
2020-10-13 19:30:33 +0300

    Scripted auto-commit on change (2020-10-13 19:30:33) by gitwatch.sh

--- src/main/java/flight/WComparable.java
+++ src/main/java/flight/WComparable.java
@@ -3,6 +3,7 @@ package flight;
 import org.apache.hadoop.io.WritableComparable;
 
 import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
 
 public class WComparable implements WritableComparable {
@@ -11,6 +12,11 @@ public class WComparable implements WritableComparable {
     @Override
     public int compareTo(Object o) {
         return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
     }
 
     @Override
