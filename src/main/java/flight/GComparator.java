c0c538230c7ca1abcd833c51863a7ada17190db3
1grim1 <dima.ggrimbergg@gmail.com>
2020-10-13 19:59:51 +0300

    Scripted auto-commit on change (2020-10-13 19:59:51) by gitwatch.sh

--- src/main/java/flight/GComparator.java
+++ src/main/java/flight/GComparator.java
@@ -1,12 +1,18 @@
 package flight;
+import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.io.WritableComparable;
 
 import java.io.DataInput;
 import java.io.IOException;
 
-public class GComparator implements WritableComparable {
-    @Override
-    public void readFields(DataInput dataInput) throws IOException {
+public class GComparator extends WritableComparator {
 
+    protected GComparator(){
+        super(WComparable.class, true);
+    }
+
+    @Override
+    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+        return super.compare(b1, s1, l1, b2, s2, l2);
     }
 }
