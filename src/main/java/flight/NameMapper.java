3bb6b6aa8531eb2de097a6738412b3b38a2a3051
1grim1 <dima.ggrimbergg@gmail.com>
2020-10-13 20:10:09 +0300

    Scripted auto-commit on change (2020-10-13 20:10:09) by gitwatch.sh

--- src/main/java/flight/NameMapper.java
+++ src/main/java/flight/NameMapper.java
@@ -4,9 +4,12 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class NameMapper extends Mapper<LongWritable, Text, WComparable, Text> {
 
-    public NameMapper() {
-        super();
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
     }
 }
