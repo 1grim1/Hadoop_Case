698bfb66f4f5fc2d1f41c240833696d059d92a9c
1grim1 <dima.ggrimbergg@gmail.com>
2020-10-13 20:05:44 +0300

    Scripted auto-commit on change (2020-10-13 20:05:44) by gitwatch.sh

--- src/main/java/flight/FlightPartitioner.java
+++ src/main/java/flight/FlightPartitioner.java
@@ -1,7 +1,13 @@
 package flight;
 
+import org.apache.hadoop.mapreduce.Partitioner;
+
 import javax.xml.soap.Text;
 
 public class FlightPartitioner extends Partitioner<WComparable, Text> {
 
+    @Override
+    public int getPartition(WComparable wComparable, Text text, int i) {
+        return 0;
+    }
 }
