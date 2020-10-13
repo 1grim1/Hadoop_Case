3c4879cad931d2904b4ddb8e19364598d4500e0b
1grim1 <dima.ggrimbergg@gmail.com>
2020-10-13 17:41:51 +0300

    Scripted auto-commit on change (2020-10-13 17:41:51) by gitwatch.sh

--- src/main/java/flight/FlightJob.java
+++ src/main/java/flight/FlightJob.java
@@ -1,11 +1,13 @@
 package flight;
 
+import org.apache.hadoop.mapreduce.Job;
+
 public class FlightJob {
     public static void main(String[] args){
         if(args.length != 3){
             System.exit(-1);
         }
 
-        Job job
+        Job job = Job.getInstance();
     }
 }
