7bc9924b8f604cc6ae94bdfb42402c5c46e809a1
1grim1 <dima.ggrimbergg@gmail.com>
2020-10-13 19:26:57 +0300

    Scripted auto-commit on change (2020-10-13 19:26:57) by gitwatch.sh

--- src/main/java/flight/FlightMapper.java
+++ src/main/java/flight/FlightMapper.java
@@ -11,14 +11,14 @@ public class FlightMapper extends Mapper<LongWritable, Text, WritableComparable,
 
     private static final String SEPARATOR = ",";
 
-    private static final int AIRPORT_ID_LEN = 14;
+    private static final int DEST_AIRPORT_ID_LEN = 14;
 
     private static final int AIRPORT_DELAY_LEN = 17;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] table = value.toString().split(SEPARATOR);
-        int airportID = Integer.parseInt(table[AIRPORT_ID_LEN]);
+        int airportID = Integer.parseInt(table[DEST_AIRPORT_ID_LEN]);
         double airportDelay = getDelay(table[AIRPORT_DELAY_LEN]);
         if(airportDelay > 0.0){
             WritableComparable comparable = new WritableComparable(airportID, 1);
