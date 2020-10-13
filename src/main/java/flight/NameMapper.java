519d7e6aa9b4eb0fd37d677c3930be29550a99eb
1grim1 <dima.ggrimbergg@gmail.com>
2020-10-13 20:09:58 +0300

    Scripted auto-commit on change (2020-10-13 20:09:58) by gitwatch.sh

--- src/main/java/flight/NameMapper.java
+++ src/main/java/flight/NameMapper.java
@@ -1,4 +1,12 @@
 package flight;
 
-public class NameMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class NameMapper extends Mapper<LongWritable, Text, WComparable, Text> {
+
+    public NameMapper() {
+        super();
+    }
 }
