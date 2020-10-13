64b9d0263708f5ec184c7f1bacb713b77c3a2f36
1grim1 <dima.ggrimbergg@gmail.com>
2020-10-13 20:19:21 +0300

    Scripted auto-commit on change (2020-10-13 20:19:21) by gitwatch.sh

--- src/main/java/flight/FlightReduce.java
+++ src/main/java/flight/FlightReduce.java
@@ -1,7 +1,15 @@
 package flight;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class FlightReduce extends Reducer<WComparable key, Text, Text, Text>{
+import java.io.IOException;
 
+public class FlightReduce extends Reducer<WComparable, Text, Text, Text>{
+    private final int COUNT = 0;
+
+    @Override
+    protected void reduce(WComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        super.reduce(key, values, context);
+    }
 }
