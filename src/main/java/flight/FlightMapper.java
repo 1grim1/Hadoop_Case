f035982f79b975dcd642b6e8700fe56aa88f37d7
1grim1 <52464868+1grim1@users.noreply.github.com>
2020-10-16 19:25:41 +0300

    Update FlightMapper.java

--- src/main/java/flight/FlightMapper.java
+++ src/main/java/flight/FlightMapper.java
@@ -12,17 +12,18 @@ public class FlightMapper extends Mapper<LongWritable, Text, WComparable,  Text>
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String SEPARATOR = ",";
-        String[] table = value.toString().split(SEPARATOR);
-        int DEST_AIRPORT_ID_LEN = 14;
+        if(key.get() != 0){
+            String[] table = value.toString().split(SEPARATOR);
+            int DEST_AIRPORT_ID_LEN = 14;
 
-        int airportID = Integer.parseInt(table[DEST_AIRPORT_ID_LEN]);
-        int ARR_DELAY_LEN = 17;
-        double airportDelay = getDelay(table[ARR_DELAY_LEN]);
-        if(airportDelay > 0.0){
-            WComparable comparable = new WComparable(airportID, 1);
-            context.write(comparable, new  Text(Double.toString(airportDelay)));
-        }
-
+            int airportID = Integer.parseInt(table[DEST_AIRPORT_ID_LEN]);
+            int ARR_DELAY_LEN = 17;
+            double airportDelay = getDelay(table[ARR_DELAY_LEN]);
+            if(airportDelay > 0.0){
+                WComparable comparable = new WComparable(airportID, 1);
+                context.write(comparable, new  Text(Double.toString(airportDelay)));
+            }
+       }
     }
 
     private double getDelay(String value) { return value.equals("") ? 0.0 : Double.parseDouble(value);}
